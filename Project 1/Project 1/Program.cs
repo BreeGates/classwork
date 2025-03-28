b5a620c8213e6a9e118e120f53e5e046faaab9c7
Your Name <your-email@example.com>
2025-03-27 22:12:47 -0400

    some message

---
+++ Project 1/Project 1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+/*
+ * This is a block comment - everything between the slash asterisk and asterick slash is ignored
+ *
+ * This is useful if you have a lot to say or to deactivate code
+ * 
+ */
+namespace Project_1;
+
+class Program
+{
+    // Start of code for the class
+    //Every application has a method called Main ()
+    //a method is self-contained set code that performs a function
+    //also known as: a function or a program
+    //a method may return a value and accept data to process
+    //method signature identifies
+    // {} surround blocks of related code aka code blocks
+
+    // start of the code for Main ()
+    //This code will display whatever is between th ("") on the screen
+    
+    // return
+    // type name (data-it-accepts)
+    static void Main(string[] args)
+}
+
+Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Unit-2-Intro-To-C#/FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace FirstProgram;
+
+// This program will ask for three numbers
+//  add them together and display the total
+
+// Programming Solution - Human thinking
+// Identify the detailed steps
+// Write down each number as its given
+// Add the numbers together
+// Tell the requestor the total (display)
+//
+// Now that we have the steps in our solution
+// Identify any data/processes you need for the steps
+// 
+// 1. A place to write down each number
+// 2. A place to hold the sum/total
+// 3. A way to ask for the numbers
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Define the variables to hold the numbers we need to add
+        int number1 = 0;
+        int number2 = 0;
+        int number3 = 0;
+        
+        // Define a variable to hold the sum of the numbers
+        int sum = 0;
+        
+        Console.WriteLine("--- Starting program ---");
+
+        // Ask for the numbers at one time using the C# Console object
+        // which represents the keyboard and screen
+        Console.WriteLine("Please enter a number: "); // Asking for the number
+        string aLine; // String to hold the line of input from the keyboard
+        aLine = Console.ReadLine();   // Get a line from the keyboard
+        // We need an int value to stor our numbers
+        // Console.ReadLine() only returns a string
+        // So we need to convert the string from Console.ReadLine() to an int
+        // int. Pasrse (string) will convert a string to an int
+        number1 = int.Parse(aLine);
+        
+        
+        Console.WriteLine("--- Ending Program ---");
+    }
+}
