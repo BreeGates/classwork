c381e4342b1d2c5ce3f8266bc79044a8086318b8
Your Name <your-email@example.com>
2025-04-21 14:37:38 -0400

    lab1

--- Unit-3-Collections/Day-3-Dictionary-Example-V2/Day-2-Dictionary-Example/Program.cs
+++ Unit-3-Collections/Day-3-Dictionary-Example-V2/Day-2-Dictionary-Example/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;  // Give me access to the systems generic collections stuff
+using System.Collections.Generic;
+using System.ComponentModel.Design; // Give me access to the systems generic collections stuff
 
 namespace Day_2_Dictionary_Example
 {
@@ -10,21 +11,87 @@ namespace Day_2_Dictionary_Example
             /*********************************************************
              * Example use of a Dictionary with array/List value
              *********************************************************/
-                
+
             // Ask the user for a name and a grade
             // Store them in a Dictionary
-            
+
             // Dictionary to hold name (key) and grade (value)
             //         key     value
             //         type    type    name      = new Dictionary<key-type, value>();
             Dictionary<string, double> gradeBook = new Dictionary<string, double>();
-            
-            // Lets add 3 students - loop 3 times for-loop
-            for (int i = 0; i < 3; i++)
+
+            // Lets add students and grades until the user indicates they are done
+            // Loop until the user satisfies the condition instead of a specific number of times
+            // Replace the for loop with either a while-loop or do while()
+            // Do we want to loop at least once? Yes do while
+            //Is ok to loop 0-times? Yes - do while loop No - while-loop
+            //
+            // In this case we have to ask the user at least once to enter  some data
+            // How do we want the user to let us know they are done?
+            // Create an on/off switch
+            // Prompt - ask if they are done
+            // Set a condition based on what they tell us
+            string userResponse = "";
+            do
             {
-                // Ask the user for the student name and grade
-                Console.Write("Enter student name: ");
-                string studentName = Console.ReadLine(); // Get student name// Ask the user for the student name
+
+// Ask the user for the student name and grade
+                Console.Write("Are you done? (y/n)");
+                //Get a response from the user and convert
+                if (userResponse != "y") ;
+                userResponse = Console.ReadLine().ToLower();
+                Console.Write("Enter grade: ");
+                // We can use a try/catch to handle the exception
+                // When the user enters a non numeric grade causing N EXCEPTION
+                // 1. Ignore it and keep going - we are doing this
+                // 2. Give them another chance to enter it
+                // 3. Set the grade to 0
+
+                //We need to define grade outside any block so it can be used by all code
+                double grade = 0;
+
+                try // a statement inside this try block might cause an exception
+                {
+                    // Get the user input as a string in case we need it later
+                    userGrade = Console.ReadLine(); 
+                    grade = Double.Parse(UserGrade()); // Get student grade
+                }
+                catch (FormatException exceptionObject) // if it throws a FormatException...
+
+                {
+                    Console.WriteLine("The data you entered )(" + grade + ");
+                    Console.WriteLine("The data is ignored");
+                    //Skip adding student to the dictionary ( skip the rest of the loop process
+                    continue; // skip the rest of the loop processing
+                    
+                } 
+
+                {
+                    Console.WriteLine("");
+                    throw;
+                }
+                double grade = Double.Parse(Console.ReadLine()); // Get student grade
+
+                // Add the data to our Dictionary
+                // Dictionary[key}     = value;
+                gradeBook[studentName] = grade; // gradeBook.Add(studentName, grade) // may cause an exception
+            } while (userResponse != "n");
+
+            // Display the entrys in our Dictionary
+            // Use a KeyValuePair type to get an entry from teh Dictionary
+            //Get a response from the user and convert
+            if userResponse != "y" userResponse != "n";
+            {
+                Console.WriteLine("Sorry we only accept 'y' or 'n'.");
+                userResponse = Console.ReadLine().ToLower();
+            }
+            else
+            {
+                Console.WriteLine("Are you done? (y/n)");
+                //Get a response from the user and convert to lowercase
+                userResponse = Console.ReadLine().ToLower();
+                // We are checking userResponse AFTER we get it from the whil
+
                 Console.Write("Enter grade: ");
                 double grade = Double.Parse(Console.ReadLine()); // Get student grade
 
@@ -32,14 +99,15 @@ namespace Day_2_Dictionary_Example
                 // Dictionary[key}     = value;
                 gradeBook[studentName] = grade; // gradeBook.Add(studentName, grade) // may cause an exception
             }
-            
+
             // Display the entrys in our Dictionary
             // Use a KeyValuePair type to get an entry from teh Dictionary
+
             foreach (KeyValuePair<string, double> anEntry in gradeBook)
             {
-                Console.WriteLine(gradeBook[anEntry.Key] + " has a grade of " + anEntry.Value);
+                Console.WriteLine(anEntry.Key + " has a grade of " + anEntry.Value);
             }
-            
+
             Console.WriteLine("Please press enter to end program...");
             Console.Read();
         }
---
+++ Unit-3-Collections/Shopping List Lab/Shopping List Lab/Program.cs
@@ -0,0 +1,95 @@
+﻿
+namespace ShoppingListLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display at least 8 item names and prices.
+            // Ask the user to enter an item name
+            // If that item exists, display that item and price and add that item to the user’s order.
+            // If that item doesn’t exist, display an error and re-prompt the user.  (Display the menu again if you’d like.)
+            // After adding one to their order, ask if they want to add another. If so, repeat.  (User can enter an item more than once; we’re not keeping track of quantity at this point.)
+            // When they’re done adding items, display a list of all items ordered with prices in columns.
+            // Display the sum price of the items ordered.
+            // 
+            
+            //Display at least 8 item names and prices.
+            Dictionary<string, double> menu = new Dictionary<string, double>()
+            {
+                { "apple", 0.99 },
+                { "banana", 0.59 },
+                { "cauliflower", 1.59 },
+                { "dragonfruit", 2.19 },
+                { "elderberry", 1.79 },
+                { "figs", 2.09 },
+                { "grapefruit", 1.99 },
+                { "honeydew", 3.49 }
+            };
+
+            List<string> cart = new List<string>();
+            string answer = "y";
+
+            Console.WriteLine("Welcome to Chirpus Market!");
+
+            while (answer == "y")
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Item List");
+                Console.WriteLine(" ");
+                foreach (var item in menu)
+                {
+                    Console.WriteLine(item.Key + " - " + item.Value);
+                }
+                
+                    // Ask the user to enter an item name
+
+                Console.WriteLine();
+                Console.Write("What item would you like to order? ");
+                string input = Console.ReadLine().ToLower();
+
+                // If that item exists, display that item and price and add that item to the user’s order.
+                
+                if (menu.ContainsKey(input))
+                {
+                    Console.WriteLine("Adding " + input + " to cart at " + menu[input]);
+                    cart.Add(input);
+                }
+                else
+                {
+                    // After adding one to their order, ask if they want to add another. If so, repeat.
+                    Console.WriteLine("Sorry, we don't have that item. Please try again.");
+                    continue;
+                }
+
+                Console.Write("Would you like to order anything else (y/n)? ");
+                answer = Console.ReadLine().ToLower();
+
+                while (answer != "y" && answer != "n")
+                {
+                    Console.Write("Invalid response. Please enter 'y' or 'n': ");
+                    answer = Console.ReadLine().ToLower();
+                }
+            }
+            // When they’re done adding items, display a list of all items ordered with prices in columns
+            // Display the sum price of the items ordered
+            Console.WriteLine("");
+            Console.WriteLine("Thanks for your order!");
+            Console.WriteLine("Here's what you got:");
+
+            double total = 0;
+
+            foreach (string item in cart)
+            {
+                Console.WriteLine(item + " - " + menu[item]);
+                total += menu[item];
+            }
+
+            if (cart.Count > 0)
+            {
+                double average = total / cart.Count;
+            }
+        }
+    }
+}
+//End of Program
