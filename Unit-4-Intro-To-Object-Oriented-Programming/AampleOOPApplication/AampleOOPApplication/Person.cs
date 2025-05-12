97834c1627e8fe495fb9956f6f067892dec8e106
Your Name <your-email@example.com>
2025-05-12 11:41:55 -0400

    monday

---
+++ Unit-4-Intro-To-Object-Oriented-Programming/AampleOOPApplication/AampleOOPApplication/Person.cs
@@ -0,0 +1,35 @@
+namespace AampleOOPApplication;
+
+public class Person
+{
+    /***********
+     * This class will represent a Person
+     *
+     *A class has data (attributes) and methods (behaivor)
+     *
+     * 
+     * Attributes: Name
+     *  Height
+     * Weight
+     * Age
+     * Gender
+     * EyeColor
+     * HairColor
+     * **************
+     */
+}
+// A class is usually public so others can use it 
+
+public class Person
+{
+    /********
+     * Instance variables identify attributes unique to an object of a class
+     * **********
+     * 
+     */
+
+    private string _name; // C# coding convention is private variables start with _
+    private int _height;
+    private int _weight;
+    private string _hairColor;
+}
---
+++ Unit-4-Intro-To-Object-Oriented-Programming/AampleOOPApplication/AampleOOPApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AampleOOPApplication;
+
+class Program
+{
+    //Applicationprogram will define and manipulate objects
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Person aPerson = new Person("John", 78, 250, 6, 69, "Male", "Red", "Green");
+        Console.WriteLine(aPerson);
+    }
+}
